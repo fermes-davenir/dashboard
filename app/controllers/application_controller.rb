62471561c400a0fe0e46f4eba896766b93ea0b16
SebouChu <somniia.99@gmail.com>
2018-04-10 15:48:47 +0200

    Dashboard is here

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, only: [:dashboard, :calendar]
   protect_from_forgery with: :exception
 
   # GET /
   def home
+  end
+
+  # GET /dashboard
+  def dashboard
+  end
+
+  # GET /calendar
+  def calendar
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :planifications
-  resources :plants
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   root to: 'application#home'
+
+  resources :plants
+  get '/dashboard' => 'application#dashboard'
+  get '/calendar' => 'application#calendar'
+
 end
