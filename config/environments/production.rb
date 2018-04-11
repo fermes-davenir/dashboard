10d8be8cf5417b989e08ad38b6dd4c23f85e2377
SebouChu <somniia.99@gmail.com>
2018-04-11 10:43:23 +0200

    Production ActiveStorage Service

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  # Store files on Amazon S3.
+  config.active_storage.service = :amazon
+  
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
 
@@ -91,7 +94,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # Store files on Amazon S3.
-  config.active_storage.service = :amazon
 end
